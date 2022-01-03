1d60885e5eafbf96d9189059060749cc69da9323
Sargis <sargis.ab@gmail.com>
2022-01-03 14:50:55 -0600

    add class book

--- common_objects.py
+++ common_objects.py
@@ -30,3 +30,56 @@ class Animal:
     def color(self, new_color):
        self.__color = new_color
 
+
+
+class Book:
+    """Book class"""
+    def __init__(self, title):
+        self.__title = title
+        self.__author = 'tbd'
+        self.__publisher = "tbd"
+        self.__year = 2022
+        self.__price = 0.0
+
+    @property
+    def title(self):
+        return self.__title
+
+    @property
+    def author(self):
+        return self.__author
+
+    @property
+    def publisher(self):
+        return self.__publisher
+
+    @property
+    def year(self):
+        return self.__year
+
+    @property
+    def price(self):
+        return self.__price
+
+
+    @title.setter
+    def title(self, new_title):
+       self.__title = new_title
+    
+    @author.setter
+    def author(self, new_author):
+       self.__author = new_author
+    
+    @publisher.setter
+    def publisher(self, new_publisher):
+       self.__publisher = new_publisher
+
+    @year.setter
+    def year(self, new_year):
+       self.__year = new_year
+
+    @price.setter
+    def price(self, new_price):
+       self.__price = new_price
+
+
